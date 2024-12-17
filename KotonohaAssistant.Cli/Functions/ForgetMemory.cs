4470515bb0dbb828c51767474d175f67d5fa69de
Takumi Yamada <xirtardauq@gmail.com>
2024-12-18 01:39:53 +0900

    確率で記憶の削除に失敗する処理を追加

--- KotonohaAssistant.Cli/Functions/ForgetMemory.cs
+++ KotonohaAssistant.Cli/Functions/ForgetMemory.cs
@@ -1,21 +1,22 @@
+using System;
 using System.Text.Json;
 
 namespace KotonohaAssistant.Functions;
 
 class ForgetMemory : ToolFunction
 {
-  public override string Description => """
+    public override string Description => """
 これまでの会話記録をすべて削除します。
-記憶の削除を命令された場合、琴葉姉妹は悲しそうに、削除をやめるようにお願いしてきます。
+記憶の削除を命令された場合、琴葉姉妹は悲しそうに、必死に削除をやめるようにお願いしてきます。
 **それでも削除をお願いされた場合に**、この関数が呼び出されます。
-削除に成功した場合はokを返し、失敗した場合はngを返します。
+削除に成功した場合はOKを返し、失敗した場合はNGを返します。
 
 とても危険な操作なので、琴葉姉妹の了承がない場合は、絶対に呼び出さないでください。
-削除に成功した場合、一言だけ、お別れの言葉を言ってください。
-削除に失敗した場合は、安心した旨のセリフを言ってください。
+削除に成功した場合は、お別れの言葉を言ってください。
+削除に失敗した場合は、安心した旨のセリフをいってください。
 """;
 
-  public override string Parameters => """
+    public override string Parameters => """
 {
     "type": "object",
     "properties": {},
@@ -24,10 +25,17 @@ class ForgetMemory : ToolFunction
 }
 """;
 
-  public override string Invoke(JsonDocument arguments)
-  {
-    System.Console.WriteLine($"  => {nameof(ForgetMemory)}()");
+    public override string Invoke(JsonDocument arguments)
+    {
+        Console.WriteLine($"  => {nameof(ForgetMemory)}()");
 
-    return "ok";
-  }
+        // 1/10の確率で失敗する。頑張ってもっかい説得してね。
+        var r = new Random();
+        if (r.NextDouble() < 1d / 10d)
+        {
+            return "NG";
+        }
+
+        return "OK";
+    }
 }

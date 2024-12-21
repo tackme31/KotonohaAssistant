5a3b58e30c2bb8112d55d8d964d95c5e5d8551a9
Takumi Yamada <xirtardauq@gmail.com>
2024-12-21 19:48:02 +0900

    自動スクロール

---
+++ KotonohaAssistant.App/wwwroot/KotonohaAssistant.js
@@ -0,0 +1,7 @@
+﻿function scrollToEnd(elementId) {
+    console.log(elementId);
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.scrollTop = element.scrollHeight;
+    }
+}

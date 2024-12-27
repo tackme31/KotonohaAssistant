65255f9ea0b907b8786decef45927da3f5d30447
Takumi Yamada <xirtardauq@gmail.com>
2024-12-27 19:37:05 +0900

    スピーカーの切り替え処理を追加

--- KotonohaAssistant.VoiceServer/Program.cs
+++ KotonohaAssistant.VoiceServer/Program.cs
@@ -9,6 +9,7 @@ using KotonohaAssistant.Core.Models;
 using KotonohaAssistant.Core;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using CoreAudio;
 
 namespace KotonohaAssistant.VoiceServer
 {
@@ -23,8 +24,20 @@ namespace KotonohaAssistant.VoiceServer
         private static readonly int _waitCheckInterval = 500;
         private static readonly int _waitTimeout = 15 * 1000;
 
+        private static bool _switchSpeaker;
+        private static MMDevice _akaneDevice;
+        private static MMDevice _aoiDevice;
+
+
+
         static async Task Main(string[] args)
         {
+            // load .env
+            DotNetEnv.Env.TraversePath().Load();
+
+            // 姉妹のスピーカー切り替え
+            (_switchSpeaker, _akaneDevice, _aoiDevice) = GetSpeakerSettings();
+
             Console.WriteLine("Initializing editor host...");
             InitializeEditorHost();
 
@@ -88,6 +101,27 @@ namespace KotonohaAssistant.VoiceServer
             }
         }
 
+        private static (bool switchSpeaker, MMDevice akaneSpeaker, MMDevice aoiSpeaker) GetSpeakerSettings()
+        {
+            var switchSpeaker = bool.TryParse(Environment.GetEnvironmentVariable("SWITCH_SPEAKER") ?? throw new Exception("環境変数 'SWITCH_SPEAKER' が存在しません。"), out var v) ? v : throw new FormatException("無効な環境変数です: 'SWITCH_SPEAKER'");
+            if (!switchSpeaker)
+            {
+                return (switchSpeaker, null, null);
+            }
+
+            var deviceEnumerator = new MMDeviceEnumerator();
+            var endpoints = deviceEnumerator
+                .EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active)
+                .ToList();
+
+            var akaneSpeakerName = Environment.GetEnvironmentVariable("AKANE_SPEAKER") ?? throw new Exception("環境変数 'AKANE_SPEAKER' が存在しません。");
+            var aoiSpeakerName = Environment.GetEnvironmentVariable("AOI_SPEAKER") ?? throw new Exception("環境変数 'AOI_SPEAKER' が存在しません。");
+            var akaneDevice = endpoints.FirstOrDefault(ep => ep.DeviceInterfaceFriendlyName == akaneSpeakerName) ?? throw new Exception($"オーディオデバイス '{akaneSpeakerName}' が見つかりません。");
+            var aoiDevice = endpoints.FirstOrDefault(ep => ep.DeviceInterfaceFriendlyName == aoiSpeakerName) ?? throw new Exception($"オーディオデバイス '{aoiSpeakerName}' が見つかりません。");
+
+            return (switchSpeaker, akaneDevice, aoiDevice);
+        }
+
         private static async Task RunCommand(string command, string payload)
         {
             switch (command)
@@ -148,6 +182,7 @@ namespace KotonohaAssistant.VoiceServer
         public static async Task SpeakAsync(Kotonoha sister, string message)
         {
             EnsureEditorConnected();
+            SwitchSpeakerTo(sister);
 
             try
             {
@@ -208,6 +243,25 @@ namespace KotonohaAssistant.VoiceServer
                 }
 
                 await Task.Delay(_waitCheckInterval);
+            }
+        }
+
+        private static void SwitchSpeakerTo(Kotonoha sister)
+        {
+            if (!_switchSpeaker)
+            {
+                return;
+            }
+
+            var deviceEnumerator = new MMDeviceEnumerator();
+            switch (sister)
+            {
+                case Kotonoha.Akane:
+                    deviceEnumerator.SetDefaultAudioEndpoint(_akaneDevice);
+                    break;
+                case Kotonoha.Aoi:
+                    deviceEnumerator.SetDefaultAudioEndpoint(_aoiDevice);
+                    break;
             }
         }
     }

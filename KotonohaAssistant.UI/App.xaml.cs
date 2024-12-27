4f77857b86c8e93a21dbba1a861b4d5cdcf386a2
Takumi Yamada <xirtardauq@gmail.com>
2024-12-27 22:31:49 +0900

    KotonohaAssistant.UIプロジェクトを追加

---
+++ KotonohaAssistant.UI/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using KotonohaAssistant.Core.Models;
+using KotonohaAssistant.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO.Pipes;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KotonohaAssistant.UI
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ KotonohaAssistant.UI/MainWindow.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace KotonohaAssistant.UI
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int ReleaseDC(IntPtr hWnd, IntPtr hDc);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        private static extern bool BitBlt(IntPtr hdcDest, int xDest, int yDest, int width, int height, IntPtr hdcSrc, int xSrc, int ySrc, uint dwRop);
+
+        // EnumWindowsのコールバックデリゲート
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+
+            public int Width => Right - Left;
+            public int Height => Bottom - Top;
+        }
+
+        private enum TernaryRasterOperations : uint
+        {
+            SRCCOPY = 0x00CC0020
+        }
+
+        private DispatcherTimer _timer;
+
+        private static readonly Color AkaneColor = Color.FromArgb(255, 255, 240, 240);
+        private static readonly Color AoiColor = Color.FromArgb(255, 240, 255, 255);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // タイマーの初期化
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(1000 / 30) // 30fps
+            };
+            _timer.Tick += UpdateCapture;
+            _timer.Start();
+        }
+
+        // ウィンドウ名を列挙するメソッド
+        public static List<string> GetWindowTitles()
+        {
+            List<string> titles = new List<string>();
+
+            // EnumWindows関数を使ってウィンドウを列挙
+            EnumWindows(delegate (IntPtr hWnd, IntPtr lParam)
+            {
+                // ウィンドウのタイトルの長さを取得
+                int length = GetWindowTextLength(hWnd);
+                if (length > 0)
+                {
+                    // ウィンドウタイトルを取得
+                    StringBuilder windowTitle = new StringBuilder(length + 1);
+                    GetWindowText(hWnd, windowTitle, windowTitle.Capacity);
+
+                    // タイトルをリストに追加
+                    titles.Add(windowTitle.ToString());
+                }
+
+                // 次のウィンドウを列挙
+                return true;
+            }, IntPtr.Zero);
+
+            return titles;
+        }
+
+        private void UpdateCapture(object sender, EventArgs e)
+        {
+            // 対象ウィンドウのタイトルを指定
+            string windowTitle = "A.I.VOICE Editor - (新規プロジェクト) *";
+
+            // ウィンドウハンドルを取得
+            IntPtr hWnd = FindWindow(null, windowTitle);
+            if (hWnd == IntPtr.Zero)
+            {
+                Console.WriteLine("ウィンドウが見つかりません。");
+                return;
+            }
+
+            // ウィンドウのRECTを取得
+            if (!GetWindowRect(hWnd, out RECT rect))
+            {
+                Console.WriteLine("ウィンドウ情報の取得に失敗しました。");
+                return;
+            }
+
+            // ウィンドウDCを取得
+            IntPtr hdcWindow = GetWindowDC(hWnd);
+            if (hdcWindow == IntPtr.Zero)
+            {
+                Console.WriteLine("ウィンドウDCの取得に失敗しました。");
+                return;
+            }
+
+            int widthOffset = 10;
+            int heightOffset = 70;
+            int width = 190;
+            int height = 300;
+
+            using (var bitmap = new Bitmap(width, height))
+            {
+                bool success = false;
+
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    IntPtr hdc = g.GetHdc();
+
+                    success = BitBlt(hdc, 0, 0, width, height, hdcWindow, rect.Width - width - widthOffset, heightOffset, (uint)TernaryRasterOperations.SRCCOPY);
+                }
+
+                // WPF Imageコントロールに画像を表示
+                if (success)
+                {
+                    // (0, 0)の色でどちらが喋ってるか判断
+                    Color pixelColor = bitmap.GetPixel(0, 0);
+                    if (pixelColor == AkaneColor)
+                    {
+                        Akane.Source = ConvertBitmapToImageSource(bitmap);
+                    }
+                    if (pixelColor == AoiColor)
+                    {
+                        Aoi.Source = ConvertBitmapToImageSource(bitmap);
+                    }
+                }
+            }
+
+            // ウィンドウDCを解放
+            ReleaseDC(hWnd, hdcWindow);
+        }
+
+        private BitmapSource ConvertBitmapToImageSource(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+    }
+}

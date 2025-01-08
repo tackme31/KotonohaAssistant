b1c077221131880b419f7b1c5601896c260b7d27
Takumi Yamada <xirtardauq@gmail.com>
2025-01-09 00:26:47 +0900

    コード整理

--- KotonohaAssistant.AI/Functions/ToolFunction.cs
+++ KotonohaAssistant.AI/Functions/ToolFunction.cs
@@ -8,18 +8,38 @@ namespace KotonohaAssistant.AI.Functions;
 
 public abstract class ToolFunction(ILogger logger)
 {
-    public abstract string Description { get; }
-    public abstract string Parameters { get; }
 
     protected ILogger Logger { get; } = logger;
+
+    /// <summary>
+    /// 関数の説明
+    /// </summary>
+    public abstract string Description { get; }
+
+    /// <summary>
+    /// 関数のパラメータ
+    /// </summary>
+    public abstract string Parameters { get; }
 
     /// <summary>
     /// 怠け癖対象かどうか
     /// </summary>
     public virtual bool CanBeLazy { get; set; } = true;
 
+    /// <summary>
+    /// 引数のパース処理
+    /// </summary>
+    /// <param name="doc"></param>
+    /// <param name="arguments"></param>
+    /// <returns></returns>
     public abstract bool TryParseArguments(JsonDocument doc, out IDictionary<string, object> arguments);
 
+    /// <summary>
+    /// 関数の実行処理
+    /// </summary>
+    /// <param name="arguments"></param>
+    /// <param name="state"></param>
+    /// <returns></returns>
     public abstract Task<string> Invoke(IDictionary<string, object> arguments, IReadOnlyConversationState state);
 
     public ChatTool CreateChatTool()

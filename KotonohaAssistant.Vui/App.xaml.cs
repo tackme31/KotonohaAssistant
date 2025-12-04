f283bc888cdc151e98e27ef2cdd570a2dc71be01
Takumi Yamada <xirtardauq@gmail.com>
2025-12-04 19:43:55 +0900

    スタイル調整

--- KotonohaAssistant.Vui/App.xaml.cs
+++ KotonohaAssistant.Vui/App.xaml.cs
@@ -9,7 +9,12 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "KotonohaAssistant.Vui" };
+            return new Window(new MainPage())
+            {
+                Title = "KotonohaAssistant.Vui",
+                Width = 800,
+                Height = 1000,
+            };
         }
     }
 }

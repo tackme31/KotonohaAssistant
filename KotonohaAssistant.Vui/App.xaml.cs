09f126ca729e7d377b65f1cc23a2c20724957963
Takumi Yamada <xirtardauq@gmail.com>
2025-12-05 10:20:57 +0900

    VUIログ出力対応

--- KotonohaAssistant.Vui/App.xaml.cs
+++ KotonohaAssistant.Vui/App.xaml.cs
@@ -1,10 +1,33 @@
-﻿namespace KotonohaAssistant.Vui
+﻿using KotonohaAssistant.Core.Utils;
+using ILogger = KotonohaAssistant.Core.Utils.ILogger;
+
+namespace KotonohaAssistant.Vui
 {
     public partial class App : Application
     {
-        public App()
+        private readonly ILogger _logger;
+
+        public App(IServiceProvider serviceProvider)
         {
             InitializeComponent();
+
+            _logger = serviceProvider.GetRequiredService<ILogger>();
+
+            // 非同期タスクで発生する未処理例外
+            TaskScheduler.UnobservedTaskException += (sender, e) =>
+            {
+                LogException(e.Exception);
+                e.SetObserved();
+            };
+
+            // UIスレッドで発生する例外（主に .NET 6 / 7 の MAUI）
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                if (e.ExceptionObject is Exception ex)
+                {
+                    LogException(ex);
+                }
+            };
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
@@ -15,6 +38,11 @@
                 Width = 800,
                 Height = 1000,
             };
+        }
+
+        private void LogException(Exception ex)
+        {
+            _logger.LogError(ex);
         }
     }
 }

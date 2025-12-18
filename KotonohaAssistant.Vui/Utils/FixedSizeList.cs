44bb8b69ade5ee3298d5f332b2c9f0b68f676e4b
Takumi Yamada <xirtardauq@gmail.com>
2025-12-18 23:28:57 +0900

    VUIでメッセージの表示上限を20個に設定

---
+++ KotonohaAssistant.Vui/Utils/FixedSizeList.cs
@@ -0,0 +1,29 @@
+﻿namespace KotonohaAssistant.Vui.Utils;
+
+public class FixedSizeList<T> : IEnumerable<T>
+{
+    private readonly int _capacity;
+    private readonly Queue<T> _queue;
+
+    public FixedSizeList(int capacity)
+    {
+        _capacity = capacity;
+        _queue = new Queue<T>(capacity);
+    }
+
+    public void Add(T item)
+    {
+        _queue.Enqueue(item);
+        if (_queue.Count > _capacity)
+        {
+            _queue.Dequeue();
+        }
+    }
+
+    public int Count => _queue.Count;
+
+    public void Clear() => _queue.Clear();
+
+    public IEnumerator<T> GetEnumerator() => _queue.GetEnumerator();
+    System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+}
